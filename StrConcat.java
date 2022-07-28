3ddaedcfe4252da79444ace0481b53491c30a20f
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-07-28 02:25:30 -0800

    Add files via upload

---
+++ StrConcat.java
@@ -0,0 +1,11 @@
+package strconcat;
+
+public class StrConcat
+{
+    public static void main(String[] args)
+    {
+        String s = "Government " + "Polytechnic " + "Pune. ";
+        System.out.println(s);
+    }
+    
+}
